d18883fb1e3a1415f5808825f8ba1b674c04c590
MOMENTA\songlin.song <songlin.song@momenta.ai>
2025-01-10 19:21:28 +0800

    feat: 完善提交逻辑

--- part2/src/services/number.js
+++ part2/src/services/number.js
@@ -1,5 +1,7 @@
 import axios from 'axios'
-const baseUrl = 'http://localhost:3001/api/persons';
+// const baseUrl = 'http://localhost:3001/api/persons';
+// const baseUrl = 'https://theninetimes.onrender.com/api/persons';
+const baseUrl = '/api/persons';
 
 const getAll = () => {
   return axios
--- part2/vite.config.js
+++ part2/vite.config.js
@@ -2,6 +2,15 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
+
 export default defineConfig({
-  plugins: [react()],
-})
+  server: {
+    proxy: {
+      '/api': {
+        target: 'http://localhost:3001', // 目标后端服务器的地址
+        changeOrigin: true, // 是否改变请求的源地址
+      },
+    },
+  },
+  plugins: [react()]
+});
